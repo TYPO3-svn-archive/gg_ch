d8d6ec4fe780af5aede4674d0ba4e85fa43023c4
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-01 19:25:02 +0000

    Added base files
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/gg_ch/trunk@29588 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "gg_ch".
+#
+# Auto generated 01-02-2010 20:16
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Gemeindegrenzen CH',
+	'description' => 'This extension uses GG25 (Gemeindegrenzen) from Swiss Topo to show the national border, the cantonal boundaries, districts and communes of Switzerland on GoogleMap.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.1-dev',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Xavier Perseguers',
+	'author_email' => 'typo3@perseguers.ch',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.3.0-0.0.0',
+			'rggooglemap' => '4.1.0-0.0.0',
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+	'suggests' => array(
+	),
+);
+
+?>
